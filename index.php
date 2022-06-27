2a6a3d7988491474182aa7f906a8dc964a9040dc
Amirhossein Aliakbarpour <ah.aliakbarpour@gmail.com>
2022-06-27 10:55:31 +0430

    Create basics.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * user: AHAAP
+ * Date: 1401/4/6
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use app\core\{Application, Router};
+
+
+$app = new Application();
+
+$app->router->get('/', function () {
+    return "Hello World!";
+});
+
+$app->router->get('/contact', function () {
+    return "Contact";
+});
+
+
+$app->run();
